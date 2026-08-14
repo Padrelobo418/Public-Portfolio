4d43ce4f53ce0f509291fd6c97c4342d672a0140
Carlos Aguilar <carlos.aguilar.esparza@gmail.com>
2026-08-13 19:51:33 -0600

    add: add new exercises in ruby and theory of html request

---
+++ CodeSignal/Introduction to programming with Ruby/Programming Foundations with Ruby/4.rb
@@ -0,0 +1,9 @@
+#Complete the Missing Script
+
+puts 'Hello, Ruby!'
+
+# TODO: Comment out the 2 lines of code below using multiline comments.
+=begin puts 'Hello, world!'
+puts 'Welcome to Ruby!'
+=end
+puts 'Nice to meet you!'
---
+++ CodeSignal/Introduction to programming with Ruby/Programming Foundations with Ruby/5.rb
@@ -0,0 +1,12 @@
+#Writing a Complete Ruby Script
+
+# TODO: Use puts to print "Hello, global explorer!"
+puts 'Hello, global explorer!'
+# TODO: Add a single-line comment that says "This script prints greeting message"
+#This script prints greeting message
+# TODO: Add a multiline comment that says "Fun journey to learn Ruby programming" in 2 or more lines
+=begin
+Fun journey 
+to learn Ruby
+programming
+=end
